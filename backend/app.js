61b4a1646ea3fb885d2ea4f66482b95f591db0ff
Minshushaw18 <guptaminshu85@gmail.com>
2024-08-10 02:12:57 +0530

    add cors

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,10 @@ const app = express();
 dotenv.config();
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+  origin: [process.env.FRONTEND_URL],
+  methods: ['GET', 'POST'],
+}));
 
 const uri = process.env.MONGODB_ATLAS_URL;
 mongoose.connect(uri)
