d418108e3a49a1360891973b17f6336b1e5a33ac
Minshushaw18 <guptaminshu85@gmail.com>
2024-08-10 03:21:44 +0530

    cors bugfix

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,7 @@ app.use(cors({
   origin: 'https://student-management-vdb9.vercel.app', 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
   allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
 }));
 
 
