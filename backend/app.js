538aa54530ab10c23f32a0b7d7511d2986fbbc0d
Minshushaw18 <guptaminshu85@gmail.com>
2024-08-10 03:02:15 +0530

    add cors

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,6 @@ app.use(cors({
   origin: process.env.FRONTEND_URL, 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true, 
 }));
 
 
