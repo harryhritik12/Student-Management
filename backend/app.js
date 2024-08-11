a51611fe52462150eb6a3357462137f0a1c408cf
Minshushaw18 <guptaminshu85@gmail.com>
2024-08-11 14:58:58 +0530

    cors bugfix

--- backend/app.js
+++ backend/app.js
@@ -19,15 +19,21 @@ dotenv.config();
 app.use(express.json());
 
 
+
 app.use(cors({
-  origin: ['https://student-management-vdb9.vercel.app', 'http://localhost:3000'], 
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+  origin: ['https://student-management-vdb9.vercel.app', 'http://localhost:3000'], // should match https://student-management-vdb9.vercel.app
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
 
-
-app.options('*', cors()); // Handle preflight requests
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', ['https://student-management-vdb9.vercel.app', 'http://localhost:3000'],);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.sendStatus(200);
+});
 
 const uri = process.env.MONGODB_ATLAS_URL;
 mongoose.connect(uri)
