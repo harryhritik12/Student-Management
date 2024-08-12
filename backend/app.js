2ca75290203c6de24eb097e271ddbf270452fe18
Minshushaw18 <guptaminshu85@gmail.com>
2024-08-12 16:15:38 +0530

    changes for deployment

--- backend/app.js
+++ backend/app.js
@@ -21,14 +21,14 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin: ['https://student-management-vdb9.vercel.app', 'http://localhost:3000'], // should match https://student-management-vdb9.vercel.app
+  origin: ['http://localhost:3000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
 
 app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', ['https://student-management-vdb9.vercel.app', 'http://localhost:3000'],);
+  res.header('Access-Control-Allow-Origin', ['http://localhost:3000'],);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
