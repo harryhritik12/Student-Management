e5decf74ddd79432b8f545626ff53119515d572f
Minshushaw18 <guptaminshu85@gmail.com>
2024-08-12 18:34:24 +0530

    add cors

--- backend/app.js
+++ backend/app.js
@@ -21,14 +21,14 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin: ['http://localhost:3000'],
+  origin: ['https://campussync.netlify.app', 'http://localhost:3000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
 
 app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', ['http://localhost:3000'],);
+  res.header('Access-Control-Allow-Origin', ['https://campussync.netlify.app', 'http://localhost:3000'],);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
