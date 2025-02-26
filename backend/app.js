3b6cc7e7d8c35785d23c3b7affd92e3e11e01cd3
harryhritik <hritikpankal1999@gmail.com>
2025-02-26 20:15:12 +0530

    change backend

--- backend/app.js
+++ backend/app.js
@@ -21,14 +21,14 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin: ['https://campussync.netlify.app', 'http://localhost:3000,https://student-management-2fbi.vercel.app/'],
+  origin: ['https://campussync.netlify.app', 'http://localhost:3000','https://student-management-2fbi-git-main-hritiks-projects-bf614864.vercel.app/'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
 
 app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', ['https://campussync.netlify.app', 'http://localhost:3000','https://student-management-2fbi.vercel.app/'],);
+  res.header('Access-Control-Allow-Origin', ['https://campussync.netlify.app', 'http://localhost:3000','https://student-management-2fbi-git-main-hritiks-projects-bf614864.vercel.app/'],);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
